f4cef737be252c6e60182692e7445c0b5af27e45
smeghen <smeghen@gmail.com>
2021-05-13 15:41:08 +0000

    Added log out functionality

--- app.py
+++ app.py
@@ -73,6 +73,14 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("Successfully logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
